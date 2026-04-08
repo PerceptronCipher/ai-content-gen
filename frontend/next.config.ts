f0d44ca48c613910e8eeb7db69d8db5356b0008d
perceptroncipher <adeyemiboluwatife.olayinka@gmail.com>
2026-04-08 11:31:29 -0700

    fix: move reactCompiler out of experimental

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactCompiler: true,
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-
-  experimental: {
-    reactCompiler: true,
-  },
-  
+  reactCompiler: true,
   images: {
     remotePatterns: [
       {
